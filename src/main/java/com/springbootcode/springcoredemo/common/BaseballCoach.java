237b01b0614035877ccbeb95f81b736beb44fb4a
ShaguftaS <shaguftha.96@gmail.com>
2026-04-22 23:36:02 +0530

    Primary annotation

--- src/main/java/com/springbootcode/springcoredemo/common/BaseballCoach.java
+++ src/main/java/com/springbootcode/springcoredemo/common/BaseballCoach.java
@@ -1,8 +1,10 @@
 package com.springbootcode.springcoredemo.common;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class BaseballCoach implements Coach{
 
     @Override
