8454d00fab5f3560c5acf5e2809e975752561a8d
ShaguftaS <shaguftha.96@gmail.com>
2026-04-22 23:20:07 +0530

    Qualifiers

---
+++ src/main/java/com/springbootcode/springcoredemo/common/TrackCoach.java
@@ -0,0 +1,12 @@
+package com.springbootcode.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TrackCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "Run a hard 5k";
+    }
+}
