97c7bade42f4f7c7dad3f76b27683fc1d2282fe4
ShaguftaS <shaguftha.96@gmail.com>
2026-04-25 23:38:02 +0530

    methods for init and destroy

--- src/main/java/com/springbootcode/springcoredemo/common/CricketCoach.java
+++ src/main/java/com/springbootcode/springcoredemo/common/CricketCoach.java
@@ -1,17 +1,28 @@
 package com.springbootcode.springcoredemo.common;
 
 
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach{
 
     public CricketCoach(){
         System.out.println("In constructor: " + getClass().getSimpleName());
     }
+
+    //define our init method
+    @PostConstruct
+    public void doMyStartupStuff(){
+        System.out.println("In doMyStartupStuff();  " + getClass().getSimpleName());
+    }
+
+    //define our destroy method
+    @PostConstruct
+    public void doMyCleanUpStuff(){
+        System.out.println("In doMyCleanUpStuff();  " + getClass().getSimpleName());
+    }
+
 
     @Override
     public String getDailyWorkout() {
