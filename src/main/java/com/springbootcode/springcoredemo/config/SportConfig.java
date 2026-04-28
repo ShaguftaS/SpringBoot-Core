b16dd26c287e441ea5ccfbb8362d96592f849f0b
ShaguftaS <shaguftha.96@gmail.com>
2026-04-28 22:26:37 +0530

    Configuration

---
+++ src/main/java/com/springbootcode/springcoredemo/config/SportConfig.java
@@ -0,0 +1,15 @@
+package com.springbootcode.springcoredemo.config;
+
+import com.springbootcode.springcoredemo.common.Coach;
+import com.springbootcode.springcoredemo.common.SwimCoach;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SportConfig {
+
+    @Bean
+    public Coach swimCoach(){
+        return new SwimCoach();
+    }
+}
